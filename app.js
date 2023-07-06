86d90bb9f4926d9a6b68bf48ca64707db438456d
kmthehippie <66264446+kmthehippie@users.noreply.github.com>
2023-07-06 21:07:03 +0800

    Retrieve data and buttons function
    1. I fixed the data retrieve from local storage
    issue by myLibrary = checks for storage.
    if local storage has item myLibrary then I
    populate the storage and return the existing library.
    
    To populate storage, I retrieve data from localStorage
    then parse it. For each item in myLibrary (array), I create
    a tempBook using the tempBook function.
    Create temp book will populate the divs onto the page
    
    2. I added functionality for buttons remove read and unread
    For read or Unread btns I created QSAll and for each
    button I add an EL. If the EL hear's a click, it removes
    the current "read or unread" value and adds "unread or read"
    value. I also change myLibrary[i].read = "unread or read"
    * I realised much later that myLibrary[i] is EQUAL to
    readOrUnreadBtn[i] as they are from same position.(It does not
    automatically refresh so, we need to location.reload)
    
    Then clear storage and add the updated version of the library in.
    
    For remove btn, it was a bit trickier bcoz I forgot my array stuff
    I found that to use slice, you create 2 new arrays containing stuff
    before the index you want to remove and after the index you want to
    remove. create a final array to combine the 2 using ...frontPart,
    ...endPart. then assign that final array as myLibrary.
    Clear the storage and update the storage's myLibrary. (It does not
    automatically refresh so, we need to location.reload)
    
    3. I fixed the footer from always floating up when I opened the modal.
    I changed it to pos absolute to fix it completely.

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const library = document.querySelector(".library");
 const esc = document.querySelector(".escape");
 const mainBG = document.querySelector(".main-container");
 const form = document.querySelector("form");
-const bookRead = document.querySelector(".read");
-const bookUnread = document.querySelector(".unread"); 
 
 const bookTitleInput = document.querySelector("#book-title");
 const bookAuthorInput = document.querySelector("#book-author");
@@ -33,6 +31,14 @@ inputs.forEach(inp =>{
 })
 
 
+
+//create new book
+
+newBook.addEventListener("click", ()=>{
+    library.style.display="none";
+    mainBG.style.backgroundColor = "rgba(22, 22, 22, 0.9)";
+    form.style.display="block";
+});
 
 
 // //when the submit button pressed
@@ -67,70 +73,116 @@ form.addEventListener("submit", (e)=>{
     form.style.display="none";
     mainBG.style.backgroundColor = "rgba(22, 22, 22, 0.5)";
     
-
-    //create a Book Object using currentTitle, currentAuthor, currentPages, currentRead
-    let myTempBook = new Book(currentTitle, currentAuthor, currentPages, currentRead);
-    myTempBook.info();
-
-    //move this information into the library to be saved as an array piece
-
-    //create new div with the class book, new div textcontent, new h3 class title, new h4 class author, new p with class pages (need to add em tag here too), then need to add the buttons div and the classes read unread remove inside it. LOL omg. how to do this??
-
-    const bookCard = document.createElement("div");
-    bookCard.classList = "book";
-    //construct the contents of the book-card and add them to respective elements
-    const textContentContainer = document.createElement("div");
-    textContentContainer.classList = "text-content";
-    //construct the content of the text-content elements
-    const titleHead = document.createElement("h3");
-    titleHead.classList = "title";
-    titleHead.textContent = myTempBook.title;
-    const authorHead = document.createElement("h4");
-    authorHead.classList = "author";
-    authorHead.textContent = myTempBook.author;
-    const pagesPara = document.createElement("p");
-    pagesPara.classList = "pages";
-    pagesPara.textContent = myTempBook.pages + " pages";
-    //add them to the text-content container
-    textContentContainer.appendChild(titleHead);
-    textContentContainer.appendChild(authorHead);
-    textContentContainer.appendChild(pagesPara);
-    console.log(textContentContainer);
-    //add the textcontent to the book card.
-    bookCard.appendChild(textContentContainer);
-
-    //create div for buttons
-    const buttonsDiv = document.createElement("div");
-    buttonsDiv.classList = "buttons";
-    //choose read or unread to be displayed. created a active class to display or don't display
-    const readOrUnread = document.createElement("div");
-    if (currentRead === "read"){
-        readOrUnread.classList = "read active";
-        readOrUnread.textContent = "READ";
-    } else if (currentRead === "unread" || currentRead === "undefined"){
-        readOrUnread.classList = "unread active";
-        readOrUnread.textContent = "UNREAD";
-    }
-    buttonsDiv.appendChild(readOrUnread);
-
-    //remove div created for each card
-    const removeDiv = document.createElement("div");
-    removeDiv.classList = "remove";
-    removeDiv.textContent = "REMOVE";
-    buttonsDiv.appendChild(removeDiv);
-    
-    //add buttonsDiv to bookcard
-    bookCard.appendChild(buttonsDiv);    
-
-    //Add the card to the library (container)
-    library.appendChild(bookCard);
-
+    createTempBook(currentTitle, currentAuthor, currentPages, currentRead);
     //Need to add Book to the LIBRARY  
     addBookToLibrary(myTempBook);
-    console.log(library);
+
+        //create eventlistener for all read button unread button remove button. 
+const readOrUnreadBtn = document.querySelectorAll(".read, .unread");
+for(let i = 0; i < readOrUnreadBtn.length; i++){
+    readOrUnreadBtn[i].addEventListener("click", ()=>{
+        if(readOrUnreadBtn[i].textContent === "READ"){
+            readOrUnreadBtn[i].classList.remove("read");
+            //change the classlist to unread active and change text content to unread
+            readOrUnreadBtn[i].classList.add("unread");
+            readOrUnreadBtn[i].textContent = "UNREAD";
+            //need to change current value keyed into local storage...but HOW? get key
+            myLibrary[i].read = "unread";
+            clearStorage();
+            localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
+        } else if (readOrUnreadBtn[i].textContent === "UNREAD" ){
+            readOrUnreadBtn[i].classList.remove("unread");
+            //change the classlist to unread active and change text content to unread
+            readOrUnreadBtn[i].classList.add("read");
+            readOrUnreadBtn[i].textContent = "READ";
+            //need to change current value keyed into local storage...but HOW? get key
+            myLibrary[i].read = "read";
+            clearStorage();
+            localStorage.setItem("myLibrary", JSON.stringify(myLibrary));           
+        }
+    })
+}
+
+//clear storage
+function clearStorage(){
+localStorage.clear();
+}
+
+//create a listener for remove button and HOW TO TARGET THE SINGLE DIV TO BE REMOVED?????
+const removeBtn = document.querySelectorAll(".remove");
+    for(let i = 0; i < removeBtn.length; i++){    
+        removeBtn[i].addEventListener("click", ()=>{       
+            let frontPart = myLibrary.slice(0, [i]);
+            let endPart = myLibrary.slice([i+1]);
+            myLibrary = [...frontPart, ...endPart];
+            clearStorage();
+            localStorage.setItem("myLibrary", JSON.stringify(myLibrary)); 
+            location.reload();
+        })
+    }
+   
 })
 
+let myTempBook;
+function createTempBook(){
+ //create a Book Object using currentTitle, currentAuthor, currentPages, currentRead
+ myTempBook = new Book(currentTitle, currentAuthor, currentPages, currentRead);
 
+ //move this information into the library to be saved as an array piece
+
+ //create new div with the class book, new div textcontent, new h3 class title, new h4 class author, new p with class pages (need to add em tag here too), then need to add the buttons div and the classes read unread remove inside it. LOL omg. how to do this??
+
+ const bookCard = document.createElement("div");
+ bookCard.classList = "book";
+ //construct the contents of the book-card and add them to respective elements
+ const textContentContainer = document.createElement("div");
+ textContentContainer.classList = "text-content";
+ //construct the content of the text-content elements
+ const titleHead = document.createElement("h3");
+ titleHead.classList = "title";
+ titleHead.textContent = myTempBook.title;
+ const authorHead = document.createElement("h4");
+ authorHead.classList = "author";
+ authorHead.textContent = myTempBook.author;
+ const pagesPara = document.createElement("p");
+ pagesPara.classList = "pages";
+ pagesPara.textContent = myTempBook.pages + " pages";
+ //add them to the text-content container
+ textContentContainer.appendChild(titleHead);
+ textContentContainer.appendChild(authorHead);
+ textContentContainer.appendChild(pagesPara);
+
+ //add the textcontent to the book card.
+ bookCard.appendChild(textContentContainer);
+
+ //create div for buttons
+ const buttonsDiv = document.createElement("div");
+ buttonsDiv.classList = "buttons";
+ //choose read or unread to be displayed. created a active class to display or don't display
+ const readOrUnread = document.createElement("div");
+ if (currentRead === "read"){
+     readOrUnread.classList = "read active";
+     readOrUnread.textContent = "READ";
+ } else if (currentRead === "unread" || currentRead === "undefined"){
+     readOrUnread.classList = "unread active";
+     readOrUnread.textContent = "UNREAD";
+ }
+ buttonsDiv.appendChild(readOrUnread);
+
+ //remove div created for each card
+ const removeDiv = document.createElement("div");
+ removeDiv.classList = "remove";
+ removeDiv.textContent = "REMOVE";
+ buttonsDiv.appendChild(removeDiv);
+ 
+ //add buttonsDiv to bookcard
+ bookCard.appendChild(buttonsDiv);    
+
+ //Add the card to the library (container)
+ library.appendChild(bookCard);
+
+
+}
 
 
 // //set information from form to the object
@@ -142,22 +194,18 @@ function Book (title, author, pages, read){
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function(){
-        console.log(`${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`);
-    }
+    // this.info = function(){
+    //     console.log(`${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`);
+    // }
 };
 
-function addBookToLibrary(book){
-    myLibrary.push(book);
-    console.log(myLibrary);
-    localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
-    console.log(localStorage);
-}
+
 
 // //getItem from localstorage when starting page JSON.parse(myLibrary). if empty then run the Book function.
 function checkStorage(){
     if (localStorage.getItem("myLibrary")){  
         populateStorage();
+        return myLibrary;
     } else {
         return [];
     }
@@ -169,33 +217,61 @@ function checkStorage(){
 
 function populateStorage(){
     localStorage.getItem("myLibrary");
-    myLibrary = JSON.parse(localStorage.getItem("myLibrary"))
-
+    myLibrary = JSON.parse(localStorage.getItem("myLibrary"));
     for(item of myLibrary){
-        console.log(item);
+        currentTitle = item.title;
+        currentAuthor = item.author;
+        currentPages = item.pages;
+        currentRead = item.read;  
+        createTempBook(currentTitle, currentAuthor, currentPages, currentRead);    
     }
 }
 
+function addBookToLibrary(book){
+    myLibrary.push(book);
+    localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
+}
+//create eventlistener for all read button unread button remove button. 
+const readOrUnreadBtn = document.querySelectorAll(".read, .unread");
+    for(let i = 0; i < readOrUnreadBtn.length; i++){
+        readOrUnreadBtn[i].addEventListener("click", ()=>{
+            if(readOrUnreadBtn[i].textContent === "READ"){
+                readOrUnreadBtn[i].classList.remove("read");
+                //change the classlist to unread active and change text content to unread
+                readOrUnreadBtn[i].classList.add("unread");
+                readOrUnreadBtn[i].textContent = "UNREAD";
+                //need to change current value keyed into local storage...but HOW? get key
+                myLibrary[i].read = "unread";
+                clearStorage();
+                localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
+            } else if (readOrUnreadBtn[i].textContent === "UNREAD" ){
+                readOrUnreadBtn[i].classList.remove("unread");
+                //change the classlist to unread active and change text content to unread
+                readOrUnreadBtn[i].classList.add("read");
+                readOrUnreadBtn[i].textContent = "READ";
+                //need to change current value keyed into local storage...but HOW? get key
+                myLibrary[i].read = "read";
+                clearStorage();
+                localStorage.setItem("myLibrary", JSON.stringify(myLibrary));           
+            }
+        })
+    }
 
-//When buttons are clicked !! need to fix when no divs on page error appears for bookRead addEL bcoz bookRead dont exist
-bookRead.addEventListener("click",()=>{
-    bookRead.classList.remove = "active";
-    bookUnread.classList.add = "active";
-})
-
-bookUnread.addEventListener("click",()=>{
-    bookRead.classList.add = "active";
-    bookUnread.classList.remove = "active";
-})
-
-newBook.addEventListener("click", ()=>{
-    library.style.display="none";
-    mainBG.style.backgroundColor = "rgba(22, 22, 22, 0.9)";
-    form.style.display="block";
-});
-
-//remove function. clear out localStorage using localStorage.
-
-function emptyStorage(){
+//clear storage
+function clearStorage(){
     localStorage.clear();
 }
+
+//create a listener for remove button and HOW TO TARGET THE SINGLE DIV TO BE REMOVED?????
+const removeBtn = document.querySelectorAll(".remove");
+    for(let i = 0; i < removeBtn.length; i++){    
+        removeBtn[i].addEventListener("click", ()=>{       
+            let frontPart = myLibrary.slice(0, [i]);
+            let endPart = myLibrary.slice([i+1]);
+            myLibrary = [...frontPart, ...endPart];
+            clearStorage();
+            localStorage.setItem("myLibrary", JSON.stringify(myLibrary)); 
+            location.reload();
+        })
+    }
+
